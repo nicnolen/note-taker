d26e34422fc28bdf69cbde301f05522fd4acdd1c
Nicholas Nolen <nicnolen@ymail.com>
2022-01-27 08:20:19 -0600

    set up tests folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Note Taker Starter Code
+
+## Heroku Link 
+https://protected-basin-46289.herokuapp.com/
---
+++ utils/inputCheck.js
@@ -0,0 +1,4 @@
+// utility to check if an object has the required properties
+// e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
+
+
