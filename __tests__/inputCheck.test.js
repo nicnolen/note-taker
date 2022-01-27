77f3c5d0dcd83b4307432c1fe1f9ecd0f0ad58f9
Nicholas Nolen <nicnolen@ymail.com>
2022-01-27 08:21:08 -0600

    added failing tests to check if an object has required properties

--- __tests__/inputCheck.test.js
+++ __tests__/inputCheck.test.js
@@ -1,1 +1,17 @@
+const inputCheck = require('../utils/inputCheck');
 
+test('inputcheck() returns null when all properties exist', () => {
+  const obj = { name: 'Nic' };
+
+  expect(inputCheck(obj, 'name')).toBe(null);
+});
+
+test('inputCheck() returns an object when a property is missing', () => {
+  const obj = { name: 'Nic', ooccupation: '' };
+
+  expect(inputCheck(obj, 'name', 'occupation')).toEqual(
+    expect.objectContaining({
+      error: expect.stringContaining('No occupation specified'),
+    })
+  );
+});
